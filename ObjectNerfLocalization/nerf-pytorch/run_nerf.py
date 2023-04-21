a094116b82ec5dc3bd6d196258491355846109e1
Ubuntu <ubuntu@ip-172-31-12-40.us-east-2.compute.internal>
2023-04-21 03:33:43 +0000

    haha

--- ObjectNerfLocalization/nerf-pytorch/run_nerf.py
+++ ObjectNerfLocalization/nerf-pytorch/run_nerf.py
@@ -449,9 +449,9 @@ def config_parser():
                         help='learning rate')
     parser.add_argument("--lrate_decay", type=int, default=250, 
                         help='exponential learning rate decay (in 1000 steps)')
-    parser.add_argument("--chunk", type=int, default=1024*24, 
+    parser.add_argument("--chunk", type=int, default=1024*32, 
                         help='number of rays processed in parallel, decrease if running out of memory')
-    parser.add_argument("--netchunk", type=int, default=1024*64, 
+    parser.add_argument("--netchunk", type=int, default=1024*32, 
                         help='number of pts sent through network in parallel, decrease if running out of memory')
     parser.add_argument("--no_batching", action='store_true', 
                         help='only take random rays from 1 image at a time')
